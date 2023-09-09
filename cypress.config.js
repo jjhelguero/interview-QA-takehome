4e4a3bde26e2eea0a33f1bae787dd0429d587cbc
jjhelguero <joshua.helguero@mercari.com>
2023-09-09 11:47:44 -0500

    fix lighthouse command

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require('cypress')
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
+
 
 module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+
+      on("task", {
+        lighthouse: lighthouse(),
+      });
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'https://build-cy717a5fk.now.sh/',
