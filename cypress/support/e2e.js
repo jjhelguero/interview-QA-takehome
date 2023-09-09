ae3b49d6ff1ba84fbbcb41a6180f1f9628cfe654
jjhelguero <joshua.helguero@mercari.com>
2023-09-09 11:34:02 -0500

    update to cy12.14.0

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import '@bahmutov/cy-api/support'
+import '@bahmutov/cy-api'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
